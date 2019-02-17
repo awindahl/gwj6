103c7d02311a22a08208847ce2aeb697aa248676
Ekin Inceleme <einceleme@gmail.com>
2019-02-17 18:42:06 +0000

    Tactical AI fix

--- Game/Assets/bandit/Bandit.gd
+++ Game/Assets/bandit/Bandit.gd
@@ -12,7 +12,7 @@ var shooting = false
 export var SPEED : = 3.0
 export var FOV : = 140.0
 var isAlive = true
-export var DETECT_RADIUS : = 60.0
+export var DETECT_RADIUS : = 25.0
 export var DETECT_ROOTED_RADIUS : = 5.0
 export var BULLET_FORCE = 5.0
 
@@ -52,31 +52,29 @@ func _physics_process(delta) -> void:
 				get_parent().set_offset(get_parent().get_offset() + SPEED * delta)
 		if state == STATE_ATTACK:
 			# Rotate to player to shoot
-			if player_found_yet:
-				$AnimationPlayer.stop(true)
-				var LastRot = path_follow.rotation_degrees
-				var value = 0.2
-				value += delta
-				var LookDir = (player.global_transform.origin - path_follow.global_transform.origin) * -1
-				var RotTrans = path_follow.global_transform.looking_at(path_follow.global_transform.origin + LookDir, Vector3 (0,1,0))
-				var ThisRot : Quat = Quat(path_follow.global_transform.basis).slerp(RotTrans.basis, value)
-				ThisRot = ThisRot.normalized()
-				if value > 1:
-					value = 1
-				path_follow.set_transform(Transform(ThisRot, path_follow.translation))
-				path_follow.rotation_degrees.x = LastRot.x
-				
+			$AnimationPlayer.stop(true)
+			var LastRot = path_follow.rotation_degrees
+			var value = 0.2
+			value += delta
+			var LookDir = (player.global_transform.origin - path_follow.global_transform.origin) * -1
+			var RotTrans = path_follow.global_transform.looking_at(path_follow.global_transform.origin + LookDir, Vector3 (0,1,0))
+			var ThisRot : Quat = Quat(path_follow.global_transform.basis).slerp(RotTrans.basis, value)
+			ThisRot = ThisRot.normalized()
+			if value > 1:
+				value = 1
+			path_follow.set_transform(Transform(ThisRot, path_follow.translation))
+			path_follow.rotation_degrees.x = LastRot.x
 			if not shooting:
 				$GunTimer.start()
 				shooting = true
 				
 			if not was_player_rooted and player.IsRooted and player_found:
 				print("I can still see you...")
+				
 			if not player_found_yet and player_found:
-				print("Must have been the wind...")
-				$wind.play(0)
-				$AnimationPlayer.play("walk-loop")
-				state = STATE_PATROL
+				print("Come back here!")
+				$ResetTimer.start()
+				
 			
 		player_found = player_found_yet
 		was_player_rooted = player.IsRooted
@@ -87,8 +85,8 @@ func can_see_entity(entity) -> float:
 	var direction = entity.global_transform.origin - $RayCast.global_transform.origin
 	var distance = direction.length()
 	if distance < DETECT_RADIUS:
-		var angle_to_player = rad2deg((global_transform.basis.z).angle_to(direction.normalized()))
-		if abs(angle_to_player) < FOV/2:
+		var angle_to_player = rad2deg(acos(direction.normalized().dot(path_follow.global_transform.basis.z)))
+		if angle_to_player < FOV/2.0:
 			$RayCast.cast_to = direction.rotated(Vector3(0,-1,0), path_follow.rotation.y)
 			if $RayCast.is_colliding() and $RayCast.get_collider() == entity:
 				return distance
@@ -98,7 +96,8 @@ func shoot(entity) -> void:
 	if isAlive:
 		$AnimationPlayer.play("shoot")
 		$shoot2.play(0)
-		entity.knockback(self, BULLET_FORCE)
+		if player_found:
+			entity.knockback(self, BULLET_FORCE)
 
 func _on_GunTimer_timeout():
 	if player.isAlive:
@@ -111,3 +110,9 @@ func _bullet_hit():
 		$AnimationPlayer.play("die")
 		$hurt.play(0)
 		$CollisionShape.disabled = true
+
+func _on_ResetTimer_timeout():
+	print("Must have been the wind...")
+	$wind.play(0)
+	$AnimationPlayer.play("walk-loop")
+	state = STATE_PATROL
