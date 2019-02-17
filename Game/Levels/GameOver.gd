6f2f511efafb9bc6d2520fca171445b9fdb91953
awindahl <awindahl@kth.se>
2019-02-17 15:57:26 +0100

    fixed game breaking bug

--- Game/Levels/GameOver.gd
+++ Game/Levels/GameOver.gd
@@ -1,4 +1,5 @@
 extends Node2D
 
 func _on_Button_pressed():
+	tracker.n = 1
 	transition.fade_to("res://Menu/menu2D.tscn", 0.9, "startSlide")
--- Game/Levels/LevelEndScreen.gd
+++ Game/Levels/LevelEndScreen.gd
@@ -1,6 +1,5 @@
 extends CanvasLayer
 
-onready var tracker = get_node("/root/tracker")
 var n = tracker.n
 
 func _on_MainMenuButton_pressed():
@@ -11,8 +10,5 @@ func _on_MainMenuButton_pressed():
 func _on_NextLevelButton_pressed():
 	tracker.n += 1
 	n = tracker.n
-	if n == 4:
-		transition.fade_to("res://Levels/YouWin.tscn", 0.9, "startSlide")
-	else: 
-		transition.fade_to("res://Levels/Level" + var2str(n) + ".tscn", 0.9, "startSlide")
-	
+	transition.fade_to("res://Levels/Level" + var2str(n) + ".tscn", 0.9, "startSlide")
+	print(n)
